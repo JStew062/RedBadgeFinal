e4ac4aa810d6f6fd0cd16c86ace45e39dd36695f
Jim Stewart <jamstew62@gmail.com>
2021-08-19 16:05:39 -0400

    3.03

---
+++ RedBadgeFinal.Data/Note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBadgeFinal.Data
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        [Display(Name = "Needed Service")]
+        public string ServiceName { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+    }
+}

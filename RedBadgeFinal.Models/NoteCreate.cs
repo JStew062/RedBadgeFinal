c1130d0e85950bbc94a9967eb3ff9efdb627dbbf
Jim Stewart <jamstew62@gmail.com>
2021-08-20 16:20:29 -0400

    5.05

---
+++ RedBadgeFinal.Models/NoteCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBadgeFinal.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        public string Content { get; set; }
+        public string ServiceNote { get; set; }
+    }
+}
